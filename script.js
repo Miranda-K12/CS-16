1f1c81187d243d04ae4583d2aae610a83b6f272b
Miranda Kachlavashvili <mirandayachlavashvili@gmail.com>
2024-07-07 23:15:19 +0400

    done exercises

--- script.js
+++ script.js
@@ -19,10 +19,11 @@ let user = {
   isloggedin: true
 }
 /*3 შექმენით ფუნქცია რომელსაც გადაეცემა ერთი პარამეტრი. ეს პარამეტრი უნდა იყოს რიცხვების მასივი და ფუნქციამ უნდა დააბრუნოს ამ რიცხვებიდან მაქსიმალური:
-(აქ მეორე ვარიანტიც შეგვიძლია დავამატოთ ამ მასალით: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max )
-
-
-
+(აქ მეორე ვარიანტიც შეგვიძლია დავამატოთ ამ მასალით: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/max ) */
+function maxNum(...array) {
+    return Math.max(...array);
+}
+console.log(maxNum(10, 2, 16, -5, 17, 60, 42)); 
 4. /*დაწერეთ ფუნქცია, რომელიც პარამეტრად მიიიღებს ერთ რიცხვს და გამოთვლის გადაცემული მნიშვნელობა კენტია თუ ლუწი;
  თუ ლუწია დააბრუნოს - this number is even‘; თუ კენტია დააბრუნოს - this number is odd‘ */
 
@@ -45,6 +46,12 @@ console.log(reverse([1, 2, 3, 4, 5]));
 /*6 ფუნქციის საშუალებით გამოთვალეთ იუსერის ასაკი ( არგუმენტად გადაეცით ნებისმიერი რიცხვი) და დაამატეთ შემდეგი პირობა, თუ გადაცემული ასაკი მეტია 18- ზე დააბრუნეთ - სრულლოვანი, თუ ნაკელებია  დააბრუნეთ არასწულწლოვანი. 
 ეს ამოცანა დაწერეთ arrow function-ით და if ის პირობა ჩაწერეთ ახალი სინტაქსით;
 */
+function isAdult(age) {
+    return age > 18 ? 'Adult' : 'Minor';
+}
+
+console.log(isAdult(23)); 
+console.log(isAdult(12));
 /*7 მოცემულია მასივი
 let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 ციკლის საშუალებით შეამოწმეთ, თუ ამ მასივში არის რიცხვი 5 - მაშინ დალოგეთ - ‘არის’ და გააჩერეთ ციკლი
@@ -92,5 +99,31 @@ for (num of array5) {
 /*11 
 Მოცემული მასივიდან ამოიღეთ მხოლოდ ციფრები, რომლებიც 5-ის ჯერადია:
 Let array7 = [32, 14, null, ‘40’, 50]; */
+let array7 = [32, 14, null, '40', 50];
+function multiple5(arr) {
+    let result = [];
+    for (let num of arr) {
+        if (typeof num === 'string') {
+            num = Number(num);
+        }
+        if (num !== null && typeof num === 'number' && num % 5 === 0) {
+            result.push(num);
+        }
+    }
+    return result;
+}
+console.log(multiple5(array7)); 
 /*12 Მოცემული მასივიდან კონსოლში გამოიტანეთ მხოლოდ დადებითი რიცხვები:
 let array8 = [ [2, -3, 5, 11], [1, -35, –11], [12, -36, -24] ]; */
+function positiveNums(arr) {
+    let result = [];
+    for (let subArray of arr) {
+        for (let num of subArray) {
+            if (num > 0) {
+                result.push(num);
+            }
+        }
+    }
+    return result;
+}
+console.log(positiveNums([[2, -3, 5, 11], [1, -35, -11], [12, -36, -24]]));
